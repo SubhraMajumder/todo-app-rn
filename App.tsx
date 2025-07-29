b8a3e16702d0656f15c77345bb7700e3572c456b
SubhraMajumder <subhramajumder32@gmail.com>
2025-07-29 19:29:48 +0530

    First commit

--- App.tsx
+++ App.tsx
@@ -1,28 +1,72 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { useState } from 'react'
+import { SafeAreaView, StatusBar, StyleSheet, TextInput, View, Text, Pressable } from 'react-native';
+// import SvgUri from 'react-native-svg-uri';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+const arr = [
+  {id: 1, name: "Subhra"},
+  {id: 2, name: "Rony"},
+  {id: 3, name: "Piu"},
+  {id: 4, name: "Rajeswari"},
+  {id: 5, name: "Bu"},
+  {id: 6, name: "Tu"}
+]
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [text, setText] = useState("");
+  // const [todo, setTodo] = useState([]);
 
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
+      <SafeAreaView>
+        <View style={styles.container}>
+          <StatusBar 
+            backgroundColor="#4000ffff"
+          />
+          <View style={styles.topview}>
+            <Text style={styles.toptitle}>Todo App</Text>
+          </View>
+
+          <View style={styles.inputcontainer}>
+            <TextInput
+              style={styles.input}
+              onChangeText={setText}
+              value={text}
+            />
+            <Pressable>
+              
+            </Pressable>
+          </View>
+
+        </View>
+      </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1
   },
+  topview: {
+    height: 100, 
+    backgroundColor: "#4000ffff"
+  },
+  toptitle: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 22,
+    textAlign: "center",
+    paddingTop: 15
+  },
+  inputcontainer: {
+    height: 100, 
+    backgroundColor: "#ffffff",
+    marginTop: -30,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25
+  },
+  input: {
+    maxWidth: "60%",
+    borderWidth: 1
+  }
 });
 
 export default App;
